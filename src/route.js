19e40f8a4df0e187a0345af3dbe71773e31260a4
Chad Smith <chad@configit.com>
2013-01-30 23:17:44 +0000

    working with empty aliases, needs code cleanup

---
+++ src/route.js
@@ -0,0 +1,231 @@
+﻿( function ( window, angular, undefined ) {
+  'use strict';
+
+  var trail = function( str, ch ) {
+    return !str.match( new RegExp( ch + '$' ) ) ? str + ch : str;
+  };
+
+  var RouteContext = function ( routeProvider ) {
+
+    // Turns action object.
+    //
+    // Examples:
+    //
+    // objectifyAction( 'member', 'details' )
+    // returns  { type: 'member', key : 'details' }
+    //
+    // objectifyAction( 'member', 'details' )
+    // returns { type: 'member', key : 'details' };
+    //
+    // objectifyAction( 'member', 'details:edit' )
+    // returns { type: 'member', key : 'details', alias:'edit' };
+    //
+    // objectifyAction( { type: 'member', alias:'edit'}, 'details' )
+    // returns { type: 'member', alias:'edit', key : 'details' };
+    var objectifyAction = function ( action, key ) {
+      var actionObj = {},
+      keyalias = key.split( ':' );
+
+      // if alias is encoded in key add it to obj and change key
+      if ( keyalias.length >= 2 ) {
+        key = keyalias[0];
+        actionObj.alias = keyalias[1];
+      }
+
+      // add string actions as type
+      if ( angular.isString( action ) ) {
+        actionObj.type = action;
+      }
+
+      // extend actionObj with object actions
+      if ( angular.isObject( action ) ) {
+        actionObj = angular.extend( actionObj, action );
+      }
+      // add key and return
+      return angular.extend( actionObj, { key: key } );
+    };
+
+    this.resource = function ( options, scopedObject ) {
+
+      var routeContext = new RouteContext( routeProvider );
+      routeContext.scopeParent = this;
+
+      routeContext.options = angular.extend( {}, this.options, options );
+
+      var resource = routeContext.options;
+      var actions = resource.actions || routeProvider.defaultActions;
+
+      var scope = resource.scope ? resource.scope + '/' : '';
+      var routePrefix = resource.routePrefix || '/';
+
+      // put collection actions first to ensure for example, that pr/new is not read as a details route (pr/:code).
+      var orderedKeys = _.sortBy( _.keys( actions ), function( key ) { return actions[key] !== 'collection'; } );
+
+      angular.forEach( orderedKeys, function ( key ) {
+        var action = objectifyAction( actions[key], key ),
+        parentName = (routeContext.scopeParent.options || {name:''}).name || '',
+        aliasedParentName = angular.isDefined((routeContext.scopeParent.options || {}).path)
+                     ? routeContext.scopeParent.options.path : (routeContext.scopeParent.options || {name:''}).name || '',
+        parentPath = ( scope + parentName ) ? trail( scope + parentName, '/' ) : '',
+        aliasedParentPath = ( scope + aliasedParentName ) ? trail( scope + aliasedParentName, '/' ) : '',
+        parentParam = !parentName ? '' : (':' + (routeContext.scopeParent.options || {name:''}).name + '_id/'),
+        resourcePath = parentPath + resource.name + '/',
+        resourceParam = ':' + angular.lowercase( resource.name ) + '_id/',
+        viewLocation = angular.lowercase( routeProvider.appRoot + resourcePath + (action.alias || action.key) + '.html' ),
+        routeWithoutKey = angular.lowercase( aliasedParentPath + parentParam + (angular.isDefined(resource.path) ? resource.path : resource.name) + '/' ).replace('//', '/');
+
+        if ( action.type === 'member' ) {
+          routeWithoutKey += resourceParam;
+        }
+
+        var routePath = angular.lowercase( routePrefix + routeWithoutKey );
+
+        var routeProperties = {
+          name: resource.name,
+          templateUrl: viewLocation,
+          reloadOnSearch: angular.isDefined( resource.reloadOnSearch ) ? resource.reloadOnSearch : true,
+          layoutUrl: resource.layoutUrl,
+          action: action.key,
+          path: resource.path || '',
+          routePrefix: routePrefix,
+          routePath: routePath,
+          isResource: true,
+          viewScope: action.type || 'member'
+        };
+
+        if ( resource.routeTransform ) {
+          resource.routeTransform( routeProperties );
+        }
+
+        routeProvider.$routeProvider.when( routePath + action.key, routeProperties );
+
+        if ( action.key === routeProvider.collectionDefaultAction || action.key === routeProvider.memberDefaultAction ) {
+          routeProvider.$routeProvider.when( routePath, {
+            redirectTo: angular.lowercase( routePath + action.key )
+          } );
+        }
+      } );
+
+      //reset the parent scope's options to ignore any actions defined on this resource
+      if ( options && options.actions ) {
+        if ( this.options && this.options.actions ) {
+          routeContext.options.actions = this.options.actions;
+        } else {
+          routeContext.options.actions = routeProvider.defaultActions;
+        }
+      }
+
+      if ( scopedObject ) {
+        scopedObject.call( routeContext );
+      }
+
+      return routeContext;
+    };
+
+    this.scope = function ( options, scopedObject ) {
+      var scopeContext = new RouteContext( routeProvider );
+      scopeContext.options = options;
+      angular.extend( scopeContext.options, this.options );
+
+      scopedObject.call( scopeContext );
+      return scopeContext;
+    };
+
+  };
+
+  var routeProvider = function () {
+    // -- config
+
+    this.$routeProvider = routeProvider.$routeProvider;
+    this.appRoot = 'client/app/';
+    this.defaultActions = {
+      'index': 'collection',
+      'new': 'collection',
+      'edit': 'member',
+      'details': 'member'
+    };
+    this.collectionDefaultAction = 'index';
+    this.memberDefaultAction = 'details';
+
+    // -- end config
+
+    this.resource = function ( options, scopedObject ) {
+      if (angular.isString(options)) 
+        options = { name: options };
+      
+      var routeContext = new RouteContext( this );
+      routeContext.resource( options, scopedObject );
+      return routeContext;
+    };
+
+    this.scope = function ( options, scopedObject ) {
+      var routeContext = new RouteContext( this );
+      routeContext.options = angular.extend( {}, options, this.options );
+
+      scopedObject.call( routeContext );
+      return routeContext;
+    };
+
+    this.$get = ['$route', '$routeParams', '$location', function ( $route, $routeParams, $location ) {
+
+      return {
+        link: function ( options ) {
+          var params = angular.extend( { }, $routeParams, options ),
+          intendedRoute = null,
+          searchTerm = '',
+          search;
+
+          if ( !options.action ) {
+            intendedRoute = $route.current;
+          } else if ( $route.current && angular.isDefined( $route.current.path ) ) {
+            intendedRoute = _.find( $route.routes, function ( r ) {
+              return angular.isDefined( r.path ) &&
+              angular.lowercase( r.path ) === angular.lowercase( $route.current.path ) &&
+              r.routePrefix === $route.current.routePrefix &&
+              r.name === $route.current.name &&
+              r.action === ( options.action || $route.current.action );
+            } );
+          }
+
+          if ( !intendedRoute ) {
+            return 'notfound?action=' + options.action;
+          }
+          // carry the search term over if the same route name
+          search = $location.search();
+          if ( !_.isEmpty( search ) && ( intendedRoute.name === $route.current.$route.name ) ) {
+            if ( intendedRoute.action === 'index' ) {
+              searchTerm = decodeURIComponent( search.back || '' );
+            }
+            else if ( _.isUndefined( search.back ) ) {
+              searchTerm = 'back=' + _.encodeUriQuery( _.toKeyValue( search ) );
+            }
+            else {
+              searchTerm = _.toKeyValue( search );
+            }
+          }
+          var path = ( intendedRoute.routePath + ( intendedRoute.action || '' ) ).
+          replace( /:([^\/]+)/ig, function ( match, group1 ) { return params[group1]; } );
+          // remove trailing '/' to ensure hrefs works when running under a url prefix (http://localhost/Ace/)
+          path = path.replace(/^\/+/, '');
+
+          return path + ( searchTerm ? '?' + searchTerm : '' );
+        },
+        action: function ( action ) {
+          return this.link( { action: action } );
+        },
+        gotoLink: function ( options ) {
+          $location.url( this.link( options ) );
+        },
+        gotoAction: function ( action ) {
+          $location.url( this.action( action ) );
+        }
+      };
+    }];
+  };
+
+  angular.module( 'cs.modules' ).config( ['$provide', '$routeProvider', function ( $provide, $routeProvider ) {
+    routeProvider.$routeProvider = $routeProvider;
+    $provide.provider( 'route', routeProvider );
+  } ] );
+
+} )( window, window.angular );
---
+++ test/routeSpec.js
@@ -0,0 +1,196 @@
+﻿describe( 'route', function () {
+  "use strict";
+
+  beforeEach( function() {
+    module( 'cs.modules' );
+  } );
+
+
+  it( 'should serve basic route to resourceful index', function() {
+    module( function( routeProvider ) {
+      routeProvider.resource( { name: 'Products' } );
+    } );
+
+    inject( function ( $location, $route, $rootScope, $httpBackend ) {
+      $httpBackend.whenGET(/.+/).respond();
+      $location.path( '/products/index' );
+      $rootScope.$digest();
+      expect( $route.current.name ).toEqual( 'Products' );
+    } );
+  } );
+
+  it( 'should serve basic route to resourceful index using short syntax', function() {
+    module( function( routeProvider ) {
+      routeProvider.resource( 'Products' );
+    } );
+
+    inject( function ( $location, $route, $rootScope, $httpBackend ) {
+      $httpBackend.whenGET(/.+/).respond();
+      $location.path( '/products/index' );
+      $rootScope.$digest();
+      expect( $route.current.name ).toEqual( 'Products' );
+    } );
+  } );
+
+  it( 'should serve basic route to resourceful index with scope', function() {
+    module( function( routeProvider ) {
+      routeProvider.resource( { name: 'Products', scope: 'admin' } );
+    } );
+
+    inject( function ( $location, $route, $rootScope, $httpBackend ) {
+      $httpBackend.whenGET(/.+/).respond();
+      $location.path( '/admin/products/index' );
+      $rootScope.$digest();
+      expect( $route.current.name ).toEqual( 'Products' );
+    } );
+  } );
+
+  it( 'should serve basic route to resourceful index with path alias', function() {
+    module( function( routeProvider ) {
+      routeProvider.resource( { name: 'Products', path: 'p' } );
+    } );
+
+    inject( function ( $location, $route, $rootScope, $httpBackend ) {
+      $httpBackend.whenGET(/.+/).respond();
+      $location.path( '/p/index' );
+      $rootScope.$digest();
+      expect( $route.current.name ).toEqual( 'Products' );
+    } );
+  } );
+
+  it( 'should serve route to nested index with path alias', function() {
+    module( function( routeProvider ) {
+      routeProvider.resource( { name: 'Lists', path: 'l' }, function () {
+        this.resource( { name: 'Items', path: 'i' } );
+      } ); 
+    } );
+
+    inject( function ( $location, $route, $rootScope, $httpBackend ) {
+      $httpBackend.whenGET(/.+/).respond();
+      $location.path( '/l/1/i/index' );
+      dump($route.routes)
+      $rootScope.$digest();
+      expect( $route.current.name ).toEqual( 'Items' );
+      expect( $route.current.templateUrl ).toEqual( 'client/app/lists/items/index.html' );
+    } );
+  } );
+
+  it( 'should serve route to nested index with empty path alias', function() {
+    module( function( routeProvider ) {
+      routeProvider.resource( { name: 'Lists' }, function () {
+        this.resource( { name: 'Items', path: '' } );
+      } ); 
+    } );
+
+    inject( function ( $location, $route, $rootScope, $httpBackend ) {
+      $httpBackend.whenGET(/.+/).respond();
+      $location.path( '/lists/1/index' );
+      $rootScope.$digest();
+      expect( $route.current.name ).toEqual( 'Items' );
+      expect( $route.current.templateUrl ).toEqual( 'client/app/lists/items/index.html' );
+    } );
+  } );
+
+  it( 'should bounce / to index', function () {
+    module( function ( routeProvider ) {
+      routeProvider.resource( { name: 'Products' } );
+    } );
+
+    inject( function ( $location, $route, $rootScope, $httpBackend ) {
+      $httpBackend.whenGET(/.+/).respond();      
+      $location.path( '/products' );
+      $rootScope.$digest();
+      expect( $location.path() ).toEqual( '/products/index' );
+    } );
+  } );
+
+  // note that products/missing is not technically incorrect as missing will become the :id parameter.
+  it( 'should not serve route to missing action', function() {
+    module( function ( routeProvider ) {
+      routeProvider.resource( { name: 'Products' } );
+    } );
+
+    inject( function( $location, $route, $rootScope, $httpBackend ) {
+      $httpBackend.whenGET(/.+/).respond();
+      $location.path( '/products/1/missing' );
+      $rootScope.$digest();
+      expect( $route.current).toBeUndefined();
+    } );
+  } );
+
+  it( 'should respond to a custom collection action', function () {
+    module( function ( routeProvider ) {
+      routeProvider.resource( { name: 'Products', actions: { custom: 'collection' } } );
+    } );
+
+    inject( function ( $location, $route, $rootScope, $httpBackend ) {
+      $httpBackend.whenGET(/.+/).respond();
+      $location.path( '/products/custom' );
+      $rootScope.$digest();
+      expect( $route.current.name ).toEqual( 'Products' );
+      expect( $route.current.action ).toEqual( 'custom' );
+    } );
+  } );
+
+  it( 'should generate collection action link from api call', function () {
+    module( function ( routeProvider ) {
+      routeProvider.resource( { name: 'Products' } );
+    } );
+
+    inject( function ( $location, $route, $rootScope, $httpBackend, route ) {
+      $httpBackend.whenGET(/.+/).respond();
+      $location.path( '/products/index' );
+      $rootScope.$digest();
+      expect( route.link( { action: 'new' } ) ).toEqual( 'products/new' );
+    } );
+  } );
+
+  it( 'should generate member action link from api call', function () {
+    module( function ( routeProvider ) {
+      routeProvider.resource( { name: 'Products' } );
+    } );
+
+    inject( function ( $location, $route, $rootScope, $httpBackend  , route ) {
+      $httpBackend.whenGET(/.+/).respond();
+      $location.path( '/products/index' );
+      $rootScope.$digest();
+      expect( route.link( { action: 'edit', products_id: 'my-product' } ) ).toEqual( 'products/my-product/edit' );
+    } );
+  } );
+
+  it( 'should be possible to specify action as objects', function () {
+    module( function ( routeProvider ) {
+      routeProvider.resource( { name: 'Products', actions: { index: 'collection', details: 'member', add: { type: 'collection' } } } );
+    } );
+
+    inject( function ( $location, $route, $rootScope, $httpBackend, route ) {
+      $httpBackend.whenGET(/.+/).respond();
+      $location.path( '/products/index' );
+      $rootScope.$digest();
+      expect( route.link( { action: 'details', products_id: 'my-product' } ) ).toEqual( 'products/my-product/details' );
+      expect( route.link( { action: 'add' } ) ).toEqual( 'products/add' );
+    } );
+  } );
+
+  it( 'should be possible to specify aliases', function () {
+    module( function ( routeProvider ) {
+      routeProvider.resource( { name: 'Products', actions: { details: 'member', add: { type: 'collection', alias : 'details' } } } );
+    } );
+
+    inject( function ( $route ) {
+      expect( $route.routes['/products/:products_id/details'].templateUrl ).toEqual( 'client/app/products/details.html' );
+      expect( $route.routes['/products/add'].templateUrl ).toEqual( 'client/app/products/details.html' );
+    } );
+  } );
+
+  it( 'should be possible to specify aliases with syntax', function () {
+    module( function ( routeProvider ) {
+      routeProvider.resource( { name: 'Products', actions: { details: 'member', 'add:details': 'collection' } } );
+    } );
+
+    inject( function ( $route ) {
+      expect( $route.routes['/products/:products_id/details'].templateUrl ).toEqual( 'client/app/products/details.html' );
+      expect( $route.routes['/products/add'].templateUrl ).toEqual( 'client/app/products/details.html' );
+    } );
+  } );
+} );
