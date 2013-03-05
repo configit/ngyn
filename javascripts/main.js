ad72add25f19953418af14db25bfb3e2c95e37c5
Chad Smith <chad@configit.com>
2013-03-05 23:18:24 +0000

    basic touch compatibilty on dropdown

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,19 @@
-console.log('This would be the main JS file.');
+$(function() {
+  
+  $(document).on('click', function(e) {
+    $('ul#navbar li.open').removeClass('open');
+  });
+
+  $('ul#navbar > li').on('click', function(e) {
+    if (!$(e.currentTarget).is('.open')) {
+      $(e.currentTarget).addClass('open');
+      e.stopPropagation();
+    }
+  });
+
+  $('ul#navbar ul li').on('click', function(e) {
+    $('ul#navbar li.open').removeClass('open');
+    e.stopPropagation();
+  });
+
+})
