c1854a58bc9bbb172b308d3142c7d01845ba8477
Chad Smith <chad@configit.com>
2013-08-21 00:20:46 +0100

    Added dropdown menu with basic positioning

--- src/ngyn-ui-multi-picker.js
+++ src/ngyn-ui-multi-picker.js
@@ -5,6 +5,22 @@ angular.module( 'ngyn-ui-multi-picker', [] )
     function dasherize(str) {
       return str.replace(/([A-Z])/g, function(v) { return '-' + angular.lowercase(v) } );
     }
+    function capitalize(str) {
+      return str.replace(/^([a-z])/, function(v) { return angular.uppercase(v) } );
+    }
+
+    function offset( elem ) {
+      var offset = {top:0,left:0};
+      do {
+        angular.forEach(['top', 'left'], function(dimension) {
+          var dimensionValue = elem['offset'+ capitalize(dimension)];
+          if ( !isNaN( dimensionValue ) ) {
+            offset[dimension] += dimensionValue;
+          }
+        });
+      } while( elem = elem.offsetParent );
+      return offset;
+    }
 
     function setInitialStyles() {
       // create an initial input element and hide it to get the props from
@@ -52,17 +68,21 @@ angular.module( 'ngyn-ui-multi-picker', [] )
       document.documentElement.removeChild(i);
     }
 
-    var myTemplate = '<span class="ngyn-picker">' +
+    var containerTemplateString = '<span class="ngyn-picker">' +
                 '  <span class="ngyn-picker-selection">' +
                 '    <span class="ngyn-picker-remove-selection">&times;</span>' +
                 '  </span>' +
                 '  <span' +
                 '    class="ngyn-picker-add-selection ngyn-picker-placeholder"' +
-                '    contenteditable ' +
+                '    contenteditable ng-focus="test"' +
                 '    > ' +
                 '    Add... '+
                 '  </span>' +
                 '</span>';
+    var menuTemplateString = '<div style="display:none" class="ngyn-picker-options">' +
+                '  <div class="ngyn-picker-option"></div>' + 
+                '</div>'
+
 
     return {
       restrict: 'E',
@@ -75,12 +95,30 @@ angular.module( 'ngyn-ui-multi-picker', [] )
         var selectionTemplate = celm.find('picker-selection');
         var optionTemplate = celm.find('picker-option');
 
-        var template = angular.element( myTemplate );
-        var selection = template.children().eq( 0 );
+        var containerElement = angular.element( containerTemplateString );
+        var selection = containerElement.children().eq( 0 );
+        var placeholder = containerElement.children().eq( 1 );
         selection.attr( 'ng-repeat', repeatableElement + ' in ' + repeatableCollection );
-        selection.prepend( selectionTemplate );
+        selection.prepend( selectionTemplate.html() );
 
-        celm.replaceWith( template );
+        var menuElement = angular.element( menuTemplateString );
+        var option = menuElement.children().eq(0);
+        option.attr( 'ng-repeat', repeatableElement + ' in ' + repeatableCollection)
+        option.prepend( optionTemplate );
+
+        celm.replaceWith();
+        celm.append(containerElement);
+        celm.append(menuElement)
+
+        placeholder.bind('focus', function() {
+          menuElement[0].style.display = 'block';
+          menuElement[0].style.left = containerElement[0].offsetLeft + 'px';
+          menuElement[0].style.top = (containerElement[0].offsetTop + containerElement[0].offsetHeight ) + 'px';
+        });
+
+        placeholder.bind('blur', function() {
+          menuElement[0].style.display = 'none';
+        });
 
         return function link( scope, elm, attrs, model ) {
         }
