0a7e31d224b7b790e76459e62558c3947b0e7a91
Chad Smith <chad@configit.com>
2014-04-07 11:21:02 +0100

    updated karma teamcity reporter patch

--- patches/karma-teamcity-reporter/index.js
+++ patches/karma-teamcity-reporter/index.js
@@ -1,5 +1,4 @@
 var util = require('util');
-var fs = require('fs');
 
 var escapeMessage = function (message) {
   if(message === null || message === undefined) {
@@ -30,8 +29,6 @@ var formatMessage = function() {
 
 var TeamcityReporter = function(baseReporterDecorator) {
   baseReporterDecorator(this);
-  
-  this.adapters = [fs.writeSync.bind(fs.writeSync, 1)];
 
   this.TEST_IGNORED  = '##teamcity[testIgnored name=\'%s\']';
   this.SUITE_START   = '##teamcity[testSuiteStarted name=\'%s\']';
@@ -39,19 +36,26 @@ var TeamcityReporter = function(baseReporterDecorator) {
   this.TEST_START    = '##teamcity[testStarted name=\'%s\']';
   this.TEST_FAILED   = '##teamcity[testFailed name=\'%s\' message=\'FAILED\' details=\'%s\']';
   this.TEST_END      = '##teamcity[testFinished name=\'%s\' duration=\'%s\']';
-  this.BROWSER_START = '##teamcity[browserStart name=\'%s\']';
-  this.BROWSER_END   = '##teamcity[browserEnd name=\'%s\']';
+  this.BLOCK_OPENED  = '##teamcity[blockOpened name=\'%s\']';
+  this.BLOCK_CLOSED  = '##teamcity[blockClosed name=\'%s\']';
+
+  var reporter = this;
+  var initializeBrowser = function(browser) {
+    reporter.browserResults[browser.id] = {
+      name: browser.name,
+      log : [],
+      lastSuite : null
+    };
+  };
 
   this.onRunStart = function(browsers) {
-    var self = this;
     this.browserResults = {};
-    browsers.forEach(function(browser) {
-      self.browserResults[browser.id] = {
-        name: browser.name,
-        log : [],
-        lastSuite : null
-      };
-    });
+    // Support Karma 0.10 (TODO: remove)
+    browsers.forEach(initializeBrowser);
+  };
+
+  this.onBrowserStart = function(browser){
+    initializeBrowser(browser);
   };
 
   this.specSuccess = function(browser, result) {
@@ -67,7 +71,7 @@ var TeamcityReporter = function(baseReporterDecorator) {
     var testName = result.description;
 
     log.push(formatMessage(this.TEST_START, testName));
-    log.push(formatMessage(this.TEST_FAILED, testName, JSON.stringify(result.log)));
+    log.push(formatMessage(this.TEST_FAILED, testName, result.log.join('\n\n')));
     log.push(formatMessage(this.TEST_END, testName, result.time));
   };
 
@@ -87,15 +91,21 @@ var TeamcityReporter = function(baseReporterDecorator) {
       if(browserResult.lastSuite) {
         log.push(formatMessage(self.SUITE_END, browserResult.lastSuite));
       }
-      self.write(formatMessage(self.BROWSER_START, browserResult.name));
+      self.write(formatMessage(self.BLOCK_OPENED, browserResult.name));
       self.write(log.join(''));
-      self.write(formatMessage(self.BROWSER_END, browserResult.name));
+      self.write(formatMessage(self.BLOCK_CLOSED, browserResult.name));
     });
   };
 
   this.getLog = function(browser, result) {
     var browserResult = this.browserResults[browser.id];
-    var suiteName = result.suite.join(' ');
+    var suiteName = browser.name;
+    var moduleName = result.suite.join(' ');
+
+    if(moduleName) {
+      suiteName = moduleName.concat('.', suiteName);
+    }
+
     var log = browserResult.log;
     if(browserResult.lastSuite !== suiteName) {
       if(browserResult.lastSuite) {
